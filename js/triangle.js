957c0d4b0199d05c615cfb6d89cea0192f55ea00
MagdaGresk <36829944+MagdaGresk@users.noreply.github.com>
2018-03-19 09:07:56 -0400

    Update scripts.js

--- js/triangle.js
+++ js/triangle.js
@@ -1,13 +1,13 @@
 var TriangleArea1 = getTriangleArea(10, 15),
 	TriangleArea2 = getTriangleArea(8, 4),
-	TriangleArea3 = getTriangleArea(10, 20);
+	TriangleArea3 = getTriangleArea(10, 20),
+	TriangleArea4 = getTriangleArea(0, 20),
+	TriangleArea5 = getTriangleArea(9, -3)
 
 function getTriangleArea(a,h) {
 	if ( a <= 0 || h <= 0 ) {
-		console.log('wrong entry');
-	}
-	else {
-		console.log(a*h/2);
+		console.log('wrong entry')
+	} else {
+		console.log(a*h/2)
 	}
 }
-getTriangleArea()
